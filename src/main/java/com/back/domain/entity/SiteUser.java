e4604b8d40344fa0794aa258710b1177e4f42d8c
1J-Choi <chldnjswp110@naver.com>
2025-08-28 16:06:06 +0900

    Spring Security 설정

--- src/main/java/com/back/domain/entity/SiteUser.java
+++ src/main/java/com/back/domain/entity/SiteUser.java
@@ -1,12 +1,14 @@
 package com.back.domain.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Setter;
 
 @Entity
 @Setter
 public class SiteUser extends BaseEntity {
+    @Column(unique = true)
     private String username;
     private String password;
 }
